97c8e9ea8483f4b354e5d66b4ed9ae2f4b422b05
OscarEl+Prron <osobando@estudiantec.cr>
2025-10-20 20:10:25 -0600

    Rehash of the Optimal algorithm

---
+++ src/algorithms/Algorithm.h
@@ -0,0 +1,11 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include ".."
+
+class Algorithm {
+   public Algorithm(){};
+   public execute()=0;
+};
+
+#endif //ALGORITHM_H
--- src/algorithms/optimal.h
+++ src/algorithms/optimal.h
@@ -9,38 +9,6 @@ int optimal(int k, List<int> &peticiones);
 
 #define OPTIMAL_IMPLMENTATION
 #include <stdio.h>
-#include "../data_structures/ArrayList.h"
-/*    peticiones = [3,1,4,8,5,9,3,2,5,4]
-    k = 2
-    cache = peticiones[:k]
-    peticiones = peticiones[k:]
-    fallos = k
-
-    for pe_i, pe in enumerate(peticiones):
-        if pe in cache:
-            print(f"HIT: {pe}")
-            continue
-
-        to_remove_pag = 0
-        to_remove = -1
-        for (pag_i, pag) in enumerate(cache):
-            if (pag not in peticiones[pe_i:]):
-                to_remove_pag = pag_i
-                break
-
-            for j in range(pe_i+1,len(peticiones)):
-                if peticiones[j] == pag:
-                    if to_remove < j:
-                           to_remove = j
-                           to_remove_pag = pag_i
-
-        cache[to_remove_pag] = pe
-        fallos+=1
-        print(f"cache:{cache}") 
-    
-    print(fallos)*
- * */
-
 int optimal(int k, List<int>& peticiones) {
     List<int>* cache = new ArrayList<int>();
     //Inserta y remueve las cosas hasta que se llene el cache
