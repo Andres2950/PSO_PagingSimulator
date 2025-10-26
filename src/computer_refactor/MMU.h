6ac3cac162d9de535c4edbdf49c50e9fef68f5ac
smaule1 <samuelzv18@gmail.com>
2025-10-25 18:32:29 -0600

    fix: actualizar diccionarios en new

--- src/computer_refactor/MMU.h
+++ src/computer_refactor/MMU.h
@@ -19,14 +19,15 @@ public:
   std::array<int, MEMORY_SIZE> memory;
   std::map<int, Page> disk;
 
-  void update_times(){
-    for(int i = 0; i < MEMORY_SIZE; ++i){
-        Page page = disk.at(memory[i]);
-        page.load_t = time - page.timestamp;
+  void update_times() {
+    for (int i = 0; i < MEMORY_SIZE; ++i) {
+      Page page = disk.at(memory[i]);
+      page.load_t = time - page.timestamp;
     }
   }
 
-  virtual int paging(Page to_insert_page)=0;
+  virtual int paging(Page to_insert_page) = 0;
+
   int _new(int pid, int size) {
     int page_ammount =
         ((size % PAGE_SIZE == 0) ? size / PAGE_SIZE : size / PAGE_SIZE + 1);
@@ -36,7 +37,6 @@ public:
     for (int i = 0; i < page_ammount; i++) {
       Page page = Page(page_id_counter++);
       page.l_addr = ptr_count;
-     
 
       // Agregar en disco
       disk[page.id] = page;
@@ -45,44 +45,47 @@ public:
       // Agregar a RAM
       bool added = false;
       for (int j = 0; j < MEMORY_SIZE; j++) {
-          if (memory[j] == -1) {
-            page.m_addr = j;
-            memory[j] = page.id;
-            added = true;
-            time += HIT_COST;
-            break;
-          }
+        if (memory[j] == -1) {
+          page.m_addr = j;
+          memory[j] = page.id;
+          added = true;
+          time += HIT_COST;
+          break;
+        }
       }
-      //paginar
+      // paginar
       if (!added) {
-          int to_remove = paging(page);
-          memory[to_remove] = page.id;
-          time += FAULT_COST;
-          fault_time += FAULT_COST;
+        int to_remove = paging(page);
+        memory[to_remove] = page.id;
+        time += FAULT_COST;
+        fault_time += FAULT_COST;
       }
       ++current_page;
       page.timestamp = time; // TODO:cambiar esto
     }
 
-    process_ptrs_map.insert(ptr_count, pages_id);
-    
+    // Asociar proceso a ptr
+    process_ptrs_map[pid].push_back(ptr_count);
+    // Asociar ptr a paginas
+    ptr_pageid_map[ptr_count] = pages_id;
+
     ptr_count++;
     update_times();
 
-    return ptr_count-1;
+    return ptr_count - 1;
   }
 
-void use(int ptr) {
+  void use(int ptr) {
     std::vector<int> pageIds = ptr_pageid_map.at(ptr);
     bool added;
-    for(int i = 0; i < pagesIds.size(); i++){
+    for (int i = 0; i < pageIds.size(); i++) {
       Page page = disk.at(pageIds[i]);
-      if(page.is_loaded){
+      if (page.is_loaded) {
         time += HIT_COST;
       } else {
         added = false;
         for (int j = 0; j < MEMORY_SIZE; j++) {
-          if(memory[j] == -1){
+          if (memory[j] == -1) {
             page.m_addr = j;
             memory[j] = page.id;
             time += HIT_COST;
@@ -91,7 +94,7 @@ void use(int ptr) {
             break;
           }
         }
-        if(!added){
+        if (!added) {
           int to_remove = paging(page);
           memory[to_remove] = page.id;
           page.timestamp = time;
