d2a55d26aba8aa85589564f29a6d1a30cc98af79
GeraldlTzy <geraldcalderon016@gmail.com>
2025-10-28 17:58:05 -0600

    Feat: reporte miedo

---
+++ report/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILE="principal"
+
+pdflatex -interaction=nonstopmode "$FILE.tex"
+bibtex "$FILE"
+pdflatex -interaction=nonstopmode "$FILE.tex"
+
+echo "COMPIALDODOPDODOODOODODDODOD"
+
