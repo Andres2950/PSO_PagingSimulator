60abf76508f26e7cd2bbd7b9b70c18b447a64f51
Gerald <126714838+GeraldlTzy@users.noreply.github.com>
2025-10-28 19:32:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -e
 
+# Get the current user and directory
 user=$(logname)
 DIR=$(pwd)
 
+# Check for root permissions
 if [ "$EUID" -ne 0 ]; then
   echo "This script requires root permissions."
   echo "It will be executed again with su -c"
   exec su -c "$0"
 fi
 
-
+# Install g++ if not installed
 install_gplusplus(){
   if command -v g++ &>/dev/null; then
     echo "G++ is already installed. Version: $(g++ --version | head -n1)"
   else
+    echo "Installing G++..."
     if command -v apt &>/dev/null; then
       apt update
       apt install -y g++
@@ -22,10 +25,12 @@ install_gplusplus(){
   fi
 }
 
+# Install cmake if not installed
 install_cmake(){
   if command -v cmake &>/dev/null; then
     echo "CMake is already installed. Version: $(cmake --version | head -n1)"
   else
+    echo "Installing CMake..."
     if command -v apt &>/dev/null; then
       apt update
       apt install -y cmake
@@ -33,26 +38,25 @@ install_cmake(){
   fi
 }
 
+# Install SDL dependencies
+install_sdl_dependencies(){
+  echo "Installing SDL dependencies..."
+  apt-get install -y libsdl2-dev libglm-dev libglfw3 libglew-dev libx11-dev
+}
 
-echo "Installign G++..."
+# installation
 install_gplusplus
-echo "Installign CMake..."
 install_cmake
-echo "Installing SDL dependencies"
-apt-get install -y libsdl2-dev libglm-dev libglfw3 libglew-dev libx11-dev
+install_sdl_dependencies
 
+# Build and run the project
+echo "Building and running the project..."
 su - "$user" -c "
-    cd '$DIR'
-    cmake -B build -S .
-    cmake --build build
-    ./build/app/app
+  export DISPLAY=:0
+  export XDG_RUNTIME_DIR=/run/user/\$(id -u)
+  export XAUTHORITY=/home/$user/.Xauthority
+  cd '$DIR'
+  cmake -B build -S .
+  cmake --build build
+  #./build/app/app
 "
-#cd "$DIR"
-#cmake -B build -S .
-#cmake --build build
-#./build/app/app
-
-# This program is compiled without root permissions
-#su "$user" -c "cd '$DIR' && make"
-#cp "$DIR/build/huff" /usr/bin
-#cp "$DIR/build/dehuff" /usr/bin
